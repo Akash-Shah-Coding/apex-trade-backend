d65fa1db454e33cf3cab8413acf63430781aea4b
Akash-Shah-Coding <codingdestini435@gmail.com>
2024-12-21 02:38:37 +0530

    testing login log

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -108,7 +108,7 @@ exports.login = async (req, res) => {
       });
     res.status(200).json({ token, user });
   } catch (err) {
-    res.status(500).json({ error: 'Error logging in' });
+    res.status(500).json({ message: 'Error logging in', error: err });
   }
 };
 
